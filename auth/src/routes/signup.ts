6fadbeaed85a783922b65050ebc124934a35f80d
Noble Obioma <nobioma1@gmail.com>
2020-05-09 22:00:47 +0100

    Add validation to request body

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,9 +1,30 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
+import { body, validationResult } from 'express-validator';
 
 const router = express.Router();
 
-router.post('/api/users/signup', (req, res) => {
-  res.send('Now you see me');
-});
+router.post(
+  '/api/users/signup',
+  [
+    body('email').isEmail().withMessage('email must be valid'),
+    body('password')
+      .trim()
+      .isLength({ min: 4, max: 20 })
+      .withMessage('Password must be between 4 and 20'),
+  ],
+  (req: Request, res: Response) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(400).send(errors.array());
+    }
+
+    const { email, password } = req.body;
+
+    console.log('creating user');
+
+    res.status(201).send({});
+  }
+);
 
 export { router as signUp };
