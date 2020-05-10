dce721aed7e528393c00aaaba8a497b53abfb1fc
Noble Obioma <nobioma1@gmail.com>
2020-05-10 09:02:07 +0100

    Add middleware to handle non-existing routes

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,10 +1,12 @@
 import express from 'express';
+import 'express-async-errors';
 
 import { currentUserRouter } from './routes/currentUser';
 import { signIn } from './routes/signin';
 import { signOut } from './routes/signout';
 import { signUp } from './routes/signup';
 import { errorHandler } from './middlewares/error-handler';
+import { NotFoundError } from './errors/not-found-error';
 
 const app = express();
 
@@ -15,6 +17,10 @@ app.use(signIn);
 app.use(signOut);
 app.use(signUp);
 app.use(errorHandler);
+
+app.all('*', async () => {
+  throw new NotFoundError();
+});
 
 app.listen(5000, () => {
   console.log('Auth service listening on PORT: 5000');
