ac4ad8f8873b1be203cd9283d00055605955eb64
Noble Obioma <nobioma1@gmail.com>
2020-05-11 11:43:06 +0100

    fix send to status for status code

--- auth/src/routes/signout.ts
+++ auth/src/routes/signout.ts
@@ -5,7 +5,7 @@ const router = express.Router();
 router.get('/api/users/signout', (req, res) => {
   req.session = null;
 
-  res.send(200).end();
+  res.status(200).end();
 });
 
 export { router as signOut };
