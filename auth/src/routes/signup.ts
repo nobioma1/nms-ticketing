cf7746d190affd0f24489addfd469f6ae6b41027
Noble Obioma <nobioma1@gmail.com>
2020-05-11 08:05:28 +0100

    Check if user exists before creatinga user and return response

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,6 +1,9 @@
 import express, { Request, Response } from 'express';
 import { body, validationResult } from 'express-validator';
+
 import { RequestValidationError } from '../errors/request-validation-error';
+import { BadRequestError } from '../errors/bad-request-error';
+import { User } from '../models/user';
 
 const router = express.Router();
 
@@ -13,7 +16,7 @@ router.post(
       .isLength({ min: 4, max: 20 })
       .withMessage('Password must be between 4 and 20'),
   ],
-  (req: Request, res: Response) => {
+  async (req: Request, res: Response) => {
     const errors = validationResult(req);
 
     if (!errors.isEmpty()) {
@@ -22,9 +25,16 @@ router.post(
 
     const { email, password } = req.body;
 
-    console.log('creating user');
+    const isExistingUser = await User.findOne({ email });
 
-    res.status(201).send({});
+    if (isExistingUser) {
+      throw new BadRequestError(`email ${email} already in use`);
+    }
+
+    const user = User.build({ email, password });
+    await user.save();
+
+    res.status(201).send(user);
   }
 );
 
