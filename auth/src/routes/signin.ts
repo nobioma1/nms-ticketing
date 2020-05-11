4b538c25393d29810456a3df6b0509a1ff4eb1e6
Noble Obioma <nobioma1@gmail.com>
2020-05-11 09:30:50 +0100

    user can signin

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,9 +1,48 @@
-import express from 'express';
+import express, { Response, Request } from 'express';
+import { body } from 'express-validator';
+
+import { validateRequest } from '../middlewares/validate-request';
+import { User } from '../models/user';
+import { BadRequestError } from '../errors/bad-request-error';
+import { Password } from '../services/password';
+import jwt from 'jsonwebtoken';
 
 const router = express.Router();
 
-router.post('/api/users/signin', (req, res) => {
-  res.send('Now you see me');
-});
+router.post(
+  '/api/users/signin',
+  [
+    body('email').isEmail().withMessage('email must be valid'),
+    body('password').trim().notEmpty().withMessage('Password is required'),
+  ],
+  validateRequest,
+  async (req: Request, res: Response) => {
+    const { email, password } = req.body;
+
+    const existingUser = await User.findOne({ email });
+
+    if (!existingUser) {
+      throw new BadRequestError('Invalid Credentials');
+    }
+
+    const passwordsMatch = Password.compare(existingUser.password, password);
+
+    if (!passwordsMatch) {
+      throw new BadRequestError('Invalid Credentials');
+    }
+
+    const token = jwt.sign(
+      {
+        id: existingUser.id,
+        email: existingUser.email,
+      },
+      process.env.JWT_KEY!
+    );
+
+    req.session = { token };
+
+    res.status(201).send(existingUser);
+  }
+);
 
 export { router as signIn };
