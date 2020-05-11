cd3f56b21978fb1ce6bf9fd72b63b9348dbb1897
Noble Obioma <nobioma1@gmail.com>
2020-05-11 11:42:23 +0100

    Add status code to res

--- auth/src/routes/currentUser.ts
+++ auth/src/routes/currentUser.ts
@@ -5,7 +5,7 @@ import { currentUser } from '../middlewares/current-user';
 const router = express.Router();
 
 router.get('/api/users/current-user', currentUser, (req, res) => {
-  res.send({ currentUser: req.currentUser || null });
+  res.status(200).send({ currentUser: req.currentUser || null });
 });
 
 export { router as currentUserRouter };
