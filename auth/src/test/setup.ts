802dee25249707fd931cafead2765e1f80e1c062
Noble Obioma <nobioma1@gmail.com>
2020-05-11 11:46:30 +0100

    Create a reusable signin function that returns an cookie and add function to global types

--- auth/src/test/setup.ts
+++ auth/src/test/setup.ts
@@ -2,6 +2,16 @@ import { MongoMemoryServer } from 'mongodb-memory-server';
 import mongoose from 'mongoose';
 import request from 'supertest';
 
+import { app } from '../app';
+
+declare global {
+  namespace NodeJS {
+    interface Global {
+      signin(): Promise<string[]>;
+    }
+  }
+}
+
 let mongo: any;
 
 beforeAll(async () => {
@@ -29,3 +39,16 @@ afterAll(async () => {
   await mongoose.connection.close();
 });
 
+global.signin = async () => {
+  const email = 'test@test.com';
+  const password = 'password';
+
+  const res = await request(app)
+    .post('/api/users/signup')
+    .send({ email, password })
+    .expect(201);
+
+  const cookie = res.get('Set-Cookie');
+
+  return cookie;
+};
