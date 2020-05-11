eb191681893459114d55694e56b873230e6425a4
Noble Obioma <nobioma1@gmail.com>
2020-05-11 10:05:06 +0100

    user can signout, clearing req session

--- auth/src/routes/signout.ts
+++ auth/src/routes/signout.ts
@@ -3,7 +3,9 @@ import express from 'express';
 const router = express.Router();
 
 router.get('/api/users/signout', (req, res) => {
-  res.send('Now you see me');
+  req.session = null;
+
+  res.send(200).end();
 });
 
 export { router as signOut };
