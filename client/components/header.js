277985abb58ec16a7f2f7d97d3538f2895bcdf05
Noble Obioma <nobioma1@gmail.com>
2020-05-12 10:45:07 +0100

    Header UI Component with buttons

---
+++ client/components/header.js
@@ -0,0 +1,27 @@
+import { Flex, Stack, Heading } from '@chakra-ui/core';
+import Link from 'next/link';
+
+import RouteLink from './route-link';
+
+const Header = (props) => {
+  return (
+    <Flex alignItems="center" justifyContent="space-between" py={3} py={2}>
+      <Link href="/">
+        <Heading cursor="pointer" fontSize="xl">
+          Tickt
+        </Heading>
+      </Link>
+
+      {!props.currentUser ? (
+        <Stack isInline>
+          <RouteLink url="/auth/signin" title="Sign In" />
+          <RouteLink url="/auth/signup" title="Sign Up" />
+        </Stack>
+      ) : (
+        <RouteLink url="/auth/signout" title="Sign Out" />
+      )}
+    </Flex>
+  );
+};
+
+export default Header;
