4d92181620b1740f532811ecb4963fe24e6f0edb
Noble Obioma <nobioma1@gmail.com>
2020-05-12 10:44:01 +0100

    ssr request

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,14 +1,34 @@
 import { ThemeProvider, CSSReset, Box } from '@chakra-ui/core';
 
-const App = ({ Component, pageProps }) => {
+import buildClient from '../api/build-client';
+import Header from '../components/header';
+
+const AppComponent = ({ Component, pageProps }) => {
   return (
     <ThemeProvider>
       <CSSReset />
       <Box px={2}>
+        <Header currentUser={pageProps.currentUser} />
         <Component {...pageProps} />
       </Box>
     </ThemeProvider>
   );
 };
 
-export default App;
+// get data during SSR process
+// can be invoked on the client or the server(routing in app) on special occasions
+AppComponent.getInitialProps = async ({ Component, ctx }) => {
+  let res = await buildClient(ctx).get('/api/users/current-user');
+
+  let pageProps = {};
+  if (Component.getInitialProps) {
+    pageProps = await Component.getInitialProps(ctx);
+  }
+
+  return {
+    pageProps,
+    ...res.data,
+  };
+};
+
+export default AppComponent;
--- client/pages/index.js
+++ client/pages/index.js
@@ -1,9 +1,21 @@
 import { Heading } from '@chakra-ui/core';
 
-export default () => {
+import buildClient from '../api/build-client';
+
+const LandingPage = ({ currentUser }) => {
   return (
-    <Heading as="h1" size="2xl">
-      Landing page
+    <Heading as="h1" size="xl">
+      {currentUser ? 'You are signed In' : 'You are not signed In'}
     </Heading>
   );
 };
+
+// get data during SSR process
+// can be invoked on the client or the server(routing in app) on special occasions
+LandingPage.getInitialProps = async (context) => {
+  let res = await buildClient(context).get('/api/users/current-user');
+
+  return res.data;
+};
+
+export default LandingPage;
