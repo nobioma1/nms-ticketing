1634389364e43828a92c4ae8c0e1cd52f4c0372e
Noble Obioma <nobioma1@gmail.com>
2020-05-12 12:41:08 +0100

    Add env checks

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -3,8 +3,16 @@ import mongoose from 'mongoose';
 import { app } from './app';
 
 const start = async () => {
+  if (!process.env.JWT_KEY) {
+    throw new Error('JWT_KEY not provided');
+  }
+
+  if (!process.env.MONGO_DB_URI) {
+    throw new Error('MONGO_DB_URI not provided');
+  }
+
   try {
-    await mongoose.connect('mongodb://tickets-mongo-service:27017/tickets', {
+    await mongoose.connect(process.env.MONGO_DB_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       useCreateIndex: true,
@@ -15,7 +23,7 @@ const start = async () => {
   }
 
   app.listen(5001, () => {
-    console.log('Auth service listening on PORT: 5001');
+    console.log('Tickets service listening on PORT: 5001');
   });
 };
 
