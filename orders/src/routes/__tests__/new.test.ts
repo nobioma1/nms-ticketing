2f9248f47652c83d206945a6b2744569326b845a
Noble Obioma <nobioma1@gmail.com>
2020-05-14 00:12:09 +0100

    user can make new order

---
+++ orders/src/routes/__tests__/new.test.ts
@@ -0,0 +1,59 @@
+import request from 'supertest';
+
+import { app } from '../../app';
+import { generateID } from '../../test/helpers/generate-id';
+import { Ticket } from '../../models/tickets';
+import { Order, OrderStatus } from '../../models/order';
+
+describe('[POST /api/orders] Create Orders', () => {
+  it('returns an error if ticket does not exist', async () => {
+    const ticketId = generateID();
+
+    const res = await request(app)
+      .post('/api/orders')
+      .set('Cookie', global.signin())
+      .send({ ticketId });
+
+    expect(res.status).toBe(404);
+  });
+
+  it('returns an error if ticket is already reserved', async () => {
+    const ticket = Ticket.build({
+      title: 'concert',
+      price: 20,
+    });
+
+    await ticket.save();
+
+    const order = Order.build({
+      ticket,
+      userId: generateID(),
+      status: OrderStatus.Created,
+      expiresAt: new Date(),
+    });
+
+    await order.save();
+
+    const res = await request(app)
+      .post('/api/orders')
+      .set('Cookie', global.signin())
+      .send({ ticketId: ticket.id });
+
+    expect(res.status).toBe(400);
+  });
+
+  it('reserves a ticket', async () => {
+    const ticket = Ticket.build({
+      title: 'concert',
+      price: 20,
+    });
+    await ticket.save();
+
+    const res = await request(app)
+      .post('/api/orders')
+      .set('Cookie', global.signin())
+      .send({ ticketId: ticket.id });
+
+    expect(res.status).toBe(201);
+  });
+});
---
+++ orders/src/routes/new.ts
@@ -0,0 +1,55 @@
+import express, { Request, Response } from 'express';
+import {
+  requireAuth,
+  validateRequest,
+  NotFoundError,
+  OrderStatus,
+  BadRequestError,
+} from '@nms-ticketing/common';
+import { body } from 'express-validator';
+
+import { Ticket } from '../models/tickets';
+import { Order } from '../models/order';
+
+const router = express.Router();
+
+const EXPIRATION_WINDOW_SECONDS = 15 * 60;
+
+router.post(
+  '/api/orders',
+  requireAuth,
+  [body('ticketId').notEmpty().withMessage('TicketId must be provided')],
+  validateRequest,
+  async (req: Request, res: Response) => {
+    const { ticketId } = req.body;
+
+    const ticket = await Ticket.findById(ticketId);
+
+    if (!ticket) {
+      throw new NotFoundError();
+    }
+
+    // find an order with ticket ID that is not cancelled
+    const isReserved = await ticket.isReserved();
+
+    if (isReserved) {
+      throw new BadRequestError('Ticket is already reserved');
+    }
+
+    // create expiration
+    const expiration = new Date();
+    expiration.setSeconds(expiration.getSeconds() + EXPIRATION_WINDOW_SECONDS);
+
+    const order = Order.build({
+      userId: req.currentUser!.id,
+      status: OrderStatus.Created,
+      expiresAt: expiration,
+      ticket,
+    });
+    await order.save();
+
+    res.status(201).send(order);
+  }
+);
+
+export { router as newOrderRouter };
