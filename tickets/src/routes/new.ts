6cdea414b77a609cb6cf3029c9c306768db5b878
Noble Obioma <nobioma1@gmail.com>
2020-05-13 20:48:56 +0100

    publish event on ticket creation

--- tickets/src/routes/new.ts
+++ tickets/src/routes/new.ts
@@ -3,6 +3,8 @@ import { requireAuth, validateRequest } from '@nms-ticketing/common';
 import { body } from 'express-validator';
 
 import { Ticket } from '../models/tickets';
+import { TicketCreatedPublisher } from '../events/publishers/ticket-created-publisher';
+import { natsWrapper } from '../nats-wrapper';
 
 const router = express.Router();
 
@@ -25,6 +27,13 @@ router.post(
       userId: req.currentUser!.id,
     });
     await ticket.save();
+
+    new TicketCreatedPublisher(natsWrapper.client).publish({
+      id: ticket.id,
+      title: ticket.title,
+      price: ticket.price,
+      userId: ticket.userId,
+    });
 
     res.status(201).send(ticket);
   }
