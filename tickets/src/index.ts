b54871649bf8fa622afc31bdeeebfb1c949481bf
Noble Obioma <nobioma1@gmail.com>
2020-05-13 20:51:26 +0100

    Set nats env variables

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -1,10 +1,23 @@
 import mongoose from 'mongoose';
 
 import { app } from './app';
+import { natsWrapper } from './nats-wrapper';
 
 const start = async () => {
   if (!process.env.JWT_KEY) {
     throw new Error('JWT_KEY not provided');
+  }
+
+  if (!process.env.NATS_CLUSTER_ID) {
+    throw new Error('NATS_CLUSTER_ID not provided');
+  }
+
+  if (!process.env.NATS_URL) {
+    throw new Error('NATS_URL not provided');
+  }
+
+  if (!process.env.NATS_CLIENT_ID) {
+    throw new Error('NATS_CLIENT_ID not provided');
   }
 
   if (!process.env.MONGO_DB_URI) {
@@ -12,6 +25,19 @@ const start = async () => {
   }
 
   try {
+    await natsWrapper.connect({
+      clusterId: process.env.NATS_CLUSTER_ID,
+      clientId: process.env.NATS_CLIENT_ID,
+      url: process.env.NATS_URL,
+    });
+
+    natsWrapper.client.on('close', () => {
+      console.log('NATS connection closed');
+      process.exit();
+    });
+    process.on('SIGINT', () => natsWrapper.client.close());
+    process.on('SIGTERM', () => natsWrapper.client.close());
+
     await mongoose.connect(process.env.MONGO_DB_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
