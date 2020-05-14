2428e14b002b8f90c57c5fda6aeed91f8e772e23
Noble Obioma <nobioma1@gmail.com>
2020-05-14 21:29:24 +0100

    Add method findByEvent to find doc by id and version and rename __v to version

--- orders/src/models/tickets.ts
+++ orders/src/models/tickets.ts
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import { updateIfCurrentPlugin } from 'mongoose-update-if-current';
 
 import { Order, OrderStatus } from './order';
 
@@ -11,14 +12,19 @@ interface TicketAttrs {
 // interface to describe properties of a user model
 interface TicketModel extends mongoose.Model<TicketDoc> {
   build(attrs: TicketAttrs): TicketDoc;
+  findByEvent(params: {
+    id: string;
+    version: number;
+  }): Promise<TicketDoc | null>;
 }
 
 // interface that describes the properties of a
 // User Document
 export interface TicketDoc extends mongoose.Document {
-  title: string;
-  price: number;
   isReserved: () => Promise<boolean>;
+  price: number;
+  title: string;
+  version: number;
 }
 
 const ticketSchema = new mongoose.Schema(
@@ -43,12 +49,29 @@ const ticketSchema = new mongoose.Schema(
   }
 );
 
+// rename __v to version
+ticketSchema.set('versionKey', 'version');
+
+// update the version property if Doc is updated
+ticketSchema.plugin(updateIfCurrentPlugin);
+
 // Adding some type checking for creating a new user
 ticketSchema.statics.build = (attrs: TicketAttrs) => {
   return new Ticket({
     _id: attrs.id,
     title: attrs.title,
     price: attrs.price,
+  });
+};
+
+// find event by the id of the doc and version
+ticketSchema.statics.findByEvent = (params: {
+  id: string;
+  version: number;
+}) => {
+  return Ticket.findOne({
+    _id: params.id,
+    version: params.version - 1,
   });
 };
 
