acd6ddc3e9382724dac2e3ecda9261471c19b5f5
Noble Obioma <nobioma1@gmail.com>
2020-05-14 21:31:43 +0100

    Pass data object containing id and version to findByEvent

--- orders/src/events/listeners/ticket-updated-listener.ts
+++ orders/src/events/listeners/ticket-updated-listener.ts
@@ -9,15 +9,17 @@ export class TicketUpdatedListener extends Listener<TicketUpdatedEvent> {
   queueGroupName = queueGroupName;
 
   async onMessage(data: TicketUpdatedEvent['data'], msg: Message) {
-    const { id, title, price } = data;
-
-    const ticket = await Ticket.findById(id);
+    const ticket = await Ticket.findByEvent(data);
 
     if (!ticket) {
       throw new Error('Ticket not found');
     }
 
-    ticket.set({ title, price });
+    ticket.set({
+      title: data.title,
+      price: data.price,
+    });
+
     await ticket.save();
 
     msg.ack();
