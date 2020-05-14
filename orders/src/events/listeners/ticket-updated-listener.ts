9d606c7b73906fc4c70d635b05866d3acd93365b
Noble Obioma <nobioma1@gmail.com>
2020-05-14 08:45:01 +0100

    Create Ticket updated listener class with method to update ticket record onMessage

---
+++ orders/src/events/listeners/ticket-updated-listener.ts
@@ -0,0 +1,25 @@
+import { Message } from 'node-nats-streaming';
+
+import { Listener, Subjects, TicketUpdatedEvent } from '@nms-ticketing/common';
+import { queueGroupName } from './queue-group-name';
+import { Ticket } from '../../models/tickets';
+
+export class TicketUpdatedListener extends Listener<TicketUpdatedEvent> {
+  subject: Subjects.TicketUpdated = Subjects.TicketUpdated;
+  queueGroupName = queueGroupName;
+
+  async onMessage(data: TicketUpdatedEvent['data'], msg: Message) {
+    const { id, title, price } = data;
+
+    const ticket = await Ticket.findById(id);
+
+    if (!ticket) {
+      throw new Error('Ticket not found');
+    }
+
+    ticket.set({ title, price });
+    await ticket.save();
+
+    msg.ack();
+  }
+}
