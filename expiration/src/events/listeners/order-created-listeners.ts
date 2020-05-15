d773d3de3ccb7508c323e0125c7a944ef6a61f55
Noble Obioma <nobioma1@gmail.com>
2020-05-15 19:53:12 +0100

    define order created listener with onMessage method to add event to queue and set delay time

---
+++ expiration/src/events/listeners/order-created-listeners.ts
@@ -0,0 +1,27 @@
+import { Message } from 'node-nats-streaming';
+import { Listener, OrderCreatedEvent, Subjects } from '@nms-ticketing/common';
+
+import { queueGroupName } from './queue-group-name';
+import { expirationQueue } from '../../queues/expiration-queue';
+
+export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
+  subject: Subjects.OrderCreated = Subjects.OrderCreated;
+  queueGroupName = queueGroupName;
+
+  async onMessage(data: OrderCreatedEvent['data'], msg: Message) {
+    // calculate time for delay
+    // delay: remaining time from now till expiresAt
+    const delay = new Date(data.expiresAt).getTime() - new Date().getTime();
+
+    await expirationQueue.add(
+      {
+        orderId: data.id,
+      },
+      {
+        delay,
+      }
+    );
+
+    msg.ack();
+  }
+}
