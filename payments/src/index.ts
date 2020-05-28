649cf2d0b57f568862c9c6f5a77503bdd99daac4
Noble Obioma <nobioma1@gmail.com>
2020-05-28 14:42:18 +0100

    Setup payments-service expresss server

---
+++ payments/src/index.ts
@@ -0,0 +1,56 @@
+import mongoose from 'mongoose';
+
+import { app } from './app';
+import { natsWrapper } from './nats-wrapper';
+
+const start = async () => {
+  if (!process.env.JWT_KEY) {
+    throw new Error('JWT_KEY not provided');
+  }
+
+  if (!process.env.NATS_CLUSTER_ID) {
+    throw new Error('NATS_CLUSTER_ID not provided');
+  }
+
+  if (!process.env.NATS_URL) {
+    throw new Error('NATS_URL not provided');
+  }
+
+  if (!process.env.NATS_CLIENT_ID) {
+    throw new Error('NATS_CLIENT_ID not provided');
+  }
+
+  if (!process.env.MONGO_DB_URI) {
+    throw new Error('MONGO_DB_URI not provided');
+  }
+
+  try {
+    await natsWrapper.connect({
+      clusterId: process.env.NATS_CLUSTER_ID,
+      clientId: process.env.NATS_CLIENT_ID,
+      url: process.env.NATS_URL,
+    });
+
+    natsWrapper.client.on('close', () => {
+      console.log('NATS connection closed');
+      process.exit();
+    });
+    process.on('SIGINT', () => natsWrapper.client.close());
+    process.on('SIGTERM', () => natsWrapper.client.close());
+
+    await mongoose.connect(process.env.MONGO_DB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    });
+    console.log('tickets-service connected to tickets-db');
+  } catch (e) {
+    console.log(e);
+  }
+
+  app.listen(5003, () => {
+    console.log('Tickets service listening on PORT: 5001');
+  });
+};
+
+start();
