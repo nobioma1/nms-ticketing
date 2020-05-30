f3aaf9c8f752e636df8e8df2c7da4197fdc5f407
Noble Obioma <nobioma1@gmail.com>
2020-05-30 23:02:13 +0100

    Add createCharge route

--- payments/src/app.ts
+++ payments/src/app.ts
@@ -1,14 +1,17 @@
-import cookieSession from 'cookie-session';
 import express from 'express';
 import 'express-async-errors';
+import cookieSession from 'cookie-session';
 import {
   errorHandler,
   NotFoundError,
   currentUser,
 } from '@nms-ticketing/common';
 
+import { createChargeRouter } from './routes/new';
+
 const app = express();
-app.set('trust proxy', true); // because traffic is coming from NGINX(a proxy)
+
+app.set('trust proxy', true);
 app.use(express.json());
 app.use(
   cookieSession({
@@ -18,11 +21,12 @@ app.use(
 );
 
 app.use(currentUser);
+app.use(createChargeRouter);
 
-app.use(errorHandler);
-
-app.all('*', async () => {
+app.all('*', async (req, res) => {
   throw new NotFoundError();
 });
+
+app.use(errorHandler);
 
 export { app };
