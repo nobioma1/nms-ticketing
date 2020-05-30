55162ea11045a1f5952033387c1d555d1f6baaa8
Noble Obioma <nobioma1@gmail.com>
2020-05-30 23:00:08 +0100

    Add message parameter

--- common/src/errors/not-found-error.ts
+++ common/src/errors/not-found-error.ts
@@ -3,13 +3,13 @@ import { CustomError } from './custom-error';
 export class NotFoundError extends CustomError {
   statusCode = 404;
 
-  constructor() {
-    super('Route not found');
+  constructor(public message: string) {
+    super(message);
 
     Object.setPrototypeOf(this, NotFoundError.prototype);
   }
 
   serializeErrors() {
-    return [{ message: 'Route not found' }];
+    return [{ message: this.message }];
   }
 }
