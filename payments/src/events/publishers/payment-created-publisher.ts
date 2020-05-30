50f614b308b1dea842282461e40b4bb15bbbe9ea
Noble Obioma <nobioma1@gmail.com>
2020-05-30 23:21:01 +0100

    Create payement created publisher

---
+++ payments/src/events/publishers/payment-created-publisher.ts
@@ -0,0 +1,9 @@
+import {
+  Subjects,
+  Publisher,
+  PaymentCreatedEvent,
+} from '@nms-ticketing/common';
+
+export class PaymentCreatedPublisher extends Publisher<PaymentCreatedEvent> {
+  subject: Subjects.PaymentCreated = Subjects.PaymentCreated;
+}
